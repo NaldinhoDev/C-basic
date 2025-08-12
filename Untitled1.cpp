c9a719a84b89d76da517af0b62126aeb140b875f
NaldinhoDev <117386614+NaldinhoDev@users.noreply.github.com>
2025-08-11 21:35:12 -0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	float n1,n2,media;
+	
+	printf ("Digite o primeiro numero: \n");
+	scanf ("%f", &n1);
+	printf ("Digite o segundo numero: \n");
+	scanf("%f", &n2);
+	
+	media = (n1+n2)/2;
+	
+	printf("Sua media e: %f", media); 
+	
+	
+}
